f12f6ce6673a00a11308e383d79d0b70764711a1
Davyd Kachor <kachor.david@gmail.com>
2022-05-02 15:29:48 +0300

    Added Redux API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector } from 'react-redux'
+
+import StartPage from './components/GameMenu/StartPage'
+import GameModePage from './components/GameMenu/GameModePage'
+import InputNamePage from './components/GameMenu/InputNamePage'
+import GameField from './components/GameField/GameField'
+import GameResult from './components/GameField/GameResult'
+import GameScoreBoard from './components/GameScoreBoard/GameScoreBoard'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const page = useSelector(state => state.ui.page)
+	const { gameMode } = useSelector(state => state.game)
+
+	let content
+
+	switch (page) {
+		default:
+			content = <StartPage />
+			break
+		case 'GAME_MODE_MENU':
+			content = <GameModePage />
+			break
+		case 'INPUT_USER_NAME_1':
+			if (gameMode === 'PLAYER-COMPUTER') {
+				content = <InputNamePage>Enter your name!</InputNamePage>
+			} else if (gameMode === 'PLAYER-PLAYER') {
+				content = <InputNamePage>Enter the name of player 1!</InputNamePage>
+			}
+			break
+		case 'INPUT_USER_NAME_2':
+			content = <InputNamePage>Enter the name of player 2!</InputNamePage>
+			break
+		case 'GAME_FIELD':
+			content = <GameField />
+			break
+		case 'GAME_RESULT':
+			content = <GameResult />
+			break
+		case 'SCOREBOARD':
+			content = <GameScoreBoard />
+			break
+	}
+
+	return content
 }
 
-export default App;
+export default App
